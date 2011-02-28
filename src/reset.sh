a94aa4ae66fd2eec9cd1e715d0b8b9c31dd9b0b7
mwclayto <mwc@clayto.org>
2011-02-27 22:40:33 -0500

    reset.sh now clears posts

--- src/reset.sh
+++ src/reset.sh
@@ -3,6 +3,7 @@
 echo Resetting database for app 'Photos'
 
 python manage.py sqlclear photos | python manage.py dbshell
+python manage.py sqlclear posts | python manage.py dbshell
 python manage.py syncdb
 
 read -p "Delete all non-hidden files/dirs in the media/photos/ directory (rm -rfv ../media/photos/*)?  (yes/n)" proceed
