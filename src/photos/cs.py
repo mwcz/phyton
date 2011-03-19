9cc9191dd1d7bdd3eab8b35ce3315f1d025d9221
mwcz <mwc@clayto.org>
2011-03-19 14:38:04 -0400

    removes PIL's palette generation; replaces with kd-means

--- src/photos/cs.py
+++ src/photos/cs.py
@@ -1,21 +1,47 @@
-#!/usr/bin/python
+from PIL import Image
+from scipy.cluster import vq
+from scipy import *
+from numpy import random
+from copy import deepcopy
 
-from PIL import Image, ImageChops
+# don't want random starting points for the kd-means centroids.
+# the palette should be the same every time it's run.
+# 13 was chosen arbitrarily.
+random.seed( 13 )
 
 def palette( _image, _size ):
+    """Calculates the 'important' colors in an image using kd-means."""
+    img = _image.copy()
+    img.thumbnail( (64,64) )            # downsize the image so kd-means will run MUCH faster.
 
-    #IMAGE_FILE    = _image     # path to image file
-    NODES         = _size      # how many swatches the palette will have
-    IMAGE         = _image.convert("RGB")
-    IMAGE_REDUCED = IMAGE.convert( "P", palette = Image.ADAPTIVE, colors = NODES ).convert("RGB")
+    pixels = list( img.getdata() )
+    wh_pixels = vq.whiten( pixels )
 
-    colors = {}
-    palette = []
+    code_book, dist = vq.kmeans( wh_pixels, 8, 20, 1e-6 )
+    #code_book, dist = vq.kmeans( wh_pixels, _size )
 
-    for (r,g,b) in IMAGE_REDUCED.getdata():
-        colors[ (r,g,b) ] = 1
+    code_ids, distortion = vq.vq( wh_pixels, code_book )
 
-    for (r,g,b) in colors:
-        palette.append( "%.2x%.2x%.2x" % ( r, g, b ) )
+    clusters = []
 
-    return tuple( palette )
+    for i in range( len( code_book ) ):
+        rgb =  px_avg( compress( code_ids == i, pixels, 0 ) )
+        clusters.append( "%.2x%.2x%.2x" % ( rgb[0], rgb[1], rgb[2] ) )
+        print(rgb,clusters[i])
+
+    # since kd-means doesn't always find exactly _size centroids,
+    # fill in any remaining centroids with black.
+    if len( clusters ) < _size:
+        for i in range( len( clusters ), _size+1 ):
+            clusters.append( "000000" )
+
+    return tuple( clusters )
+
+def px_avg( _pixels ):
+    """Accepts a list of RGB values in the form of [ (R0,G0,B0), (R1,G1,B1), ..., (Rn,Gn,Bn) ] and averages them into a single (R,G,B) tuple."""
+    num_pixels = len( _pixels )
+    r_avg = sum( [ pixel[0] for pixel in _pixels ] ) / num_pixels
+    g_avg = sum( [ pixel[1] for pixel in _pixels ] ) / num_pixels
+    b_avg = sum( [ pixel[2] for pixel in _pixels ] ) / num_pixels
+    return ( r_avg, g_avg, b_avg )
+
