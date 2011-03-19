918bfe57c04442b5b7027f288a4b38afba9f6a16
mwcz <mwc@clayto.org>
2011-03-19 19:14:53 -0400

    adds rss2 and atom1 feed support.

---
+++ src/photos/feeds.py
@@ -0,0 +1,36 @@
+from django.contrib.syndication.views import Feed
+from django.utils.feedgenerator import Atom1Feed, Rss201rev2Feed
+from photos.models import Photo
+import datetime
+
+# generic feed
+class PhotosFeed( Feed ):
+
+    title       = "clayto.com photo feed"
+    link        = "/feed/"
+    description = "a new photo each monday, wednesday, and friday"
+
+    def items( self ):
+        return Photo.objects.order_by( '-post_date' )
+
+    def item_title( self, item ):
+        return item.title
+
+    def item_description( self, item ):
+        return item.title
+
+    def item_pubdate( self, item ):
+        return datetime.datetime.combine( item.post_date, datetime.time() )
+
+    author_name = 'Michael Clayton'
+    author_url  = 'http://clayto.com/'
+    feed_copyright = 'Copyright (c) 2003-2011 Michael Clayton.  License: <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons BY-NC-SA 3.0</a>'
+    ttl = 1440
+
+class RssPhotosFeed( PhotosFeed ):
+    feed_type = Rss201rev2Feed
+    subtitle  = PhotosFeed.description
+
+class AtomPhotosFeed( PhotosFeed ):
+    feed_type = Atom1Feed
+    subtitle  = PhotosFeed.description
