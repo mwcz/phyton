ec63ceccb8d473ca9fe20b1b96c16984fb5bac3b
mwcz <mwc@clayto.org>
2011-04-07 22:15:56 -0400

    image files now saved in the correct location.

--- src/photos/forms.py
+++ src/photos/forms.py
@@ -8,42 +8,6 @@ from PIL import Image
 
 class PhotoAddForm( forms.ModelForm ):
 
-    def save(self, force_insert=False, force_update=False, commit=True):
-
-        m = super(PhotoAddForm, self).save(commit=False)
-
-        # get the image's palette
-        img = Image.open( m.image )
-        p = palette( img, 8 )
-
-        # save each color in "rgb(R,G,B)" format
-        m.suggest0 = m.palette0 = p[0]
-        m.suggest1 = m.palette1 = p[1]
-        m.suggest2 = m.palette2 = p[2]
-        m.suggest3 = m.palette3 = p[3]
-        m.suggest4 = m.palette4 = p[4]
-        m.suggest5 = m.palette5 = p[5]
-        m.suggest6 = m.palette6 = p[6]
-        m.suggest7 = m.palette7 = p[7]
-
-        # create the directory in which to place the image
-        photo_directory = os.path.join( MEDIA_ROOT, "photos", m.slug )
-        try:
-            os.mkdir( photo_directory )
-        except OSError:
-            pass # it's okay if the directory already exists; it probably can't ever happen since slugs must be unique
-
-        # resize the image
-        photo_filename = str(m.image)
-        img_path = os.path.join( MEDIA_ROOT, photo_filename )
-        img.thumbnail( IMAGE_SIZE_BOUNDS )
-        img.save( img_path )
-
-        if commit:
-            m.save()
-
-        return m
-
     class Meta:
         model  = Photo
         fields = ('image','title','slug','shot_date','post_date')
@@ -54,60 +18,23 @@ class PhotoChangeForm( PhotoAddForm ):
 
     image = forms.ImageField( label = 'PHOTO', widget = AdminImageWidget )
 
-    palette0      = forms.CharField(  label = '',      widget = AdminEditableSwatchWidget )
-    palette1      = forms.CharField(  label = '',      widget = AdminEditableSwatchWidget )
-    palette2      = forms.CharField(  label = '',      widget = AdminEditableSwatchWidget )
-    palette3      = forms.CharField(  label = '',      widget = AdminEditableSwatchWidget )
-    palette4      = forms.CharField(  label = '',      widget = AdminEditableSwatchWidget )
-    palette5      = forms.CharField(  label = '',      widget = AdminEditableSwatchWidget )
-    palette6      = forms.CharField(  label = '',      widget = AdminEditableSwatchWidget )
-    palette7      = forms.CharField(  label = '',      widget = AdminEditableSwatchWidget )
+    palette0 =   forms.CharField(  label = '',   widget = AdminEditableSwatchWidget )
+    palette1 =   forms.CharField(  label = '',   widget = AdminEditableSwatchWidget )
+    palette2 =   forms.CharField(  label = '',   widget = AdminEditableSwatchWidget )
+    palette3 =   forms.CharField(  label = '',   widget = AdminEditableSwatchWidget )
+    palette4 =   forms.CharField(  label = '',   widget = AdminEditableSwatchWidget )
+    palette5 =   forms.CharField(  label = '',   widget = AdminEditableSwatchWidget )
+    palette6 =   forms.CharField(  label = '',   widget = AdminEditableSwatchWidget )
+    palette7 =   forms.CharField(  label = '',   widget = AdminEditableSwatchWidget )
 
-    suggest0      = forms.CharField(  label = '',      widget = AdminGeneratedSwatchWidget )
-    suggest1      = forms.CharField(  label = '',      widget = AdminGeneratedSwatchWidget )
-    suggest2      = forms.CharField(  label = '',      widget = AdminGeneratedSwatchWidget )
-    suggest3      = forms.CharField(  label = '',      widget = AdminGeneratedSwatchWidget )
-    suggest4      = forms.CharField(  label = '',      widget = AdminGeneratedSwatchWidget )
-    suggest5      = forms.CharField(  label = '',      widget = AdminGeneratedSwatchWidget )
-    suggest6      = forms.CharField(  label = '',      widget = AdminGeneratedSwatchWidget )
-    suggest7      = forms.CharField(  label = '',      widget = AdminGeneratedSwatchWidget )
-
-    def save(self, force_insert=False, force_update=False, commit=True):
-
-        m = super(PhotoAddForm, self).save(commit=False)
-
-        # get the image's palette
-        img = Image.open( m.image )
-        p = palette( img, 8 )
-
-        # save each color in "rgb(R,G,B)" format
-        m.suggest0 = p[0]
-        m.suggest1 = p[1]
-        m.suggest2 = p[2]
-        m.suggest3 = p[3]
-        m.suggest4 = p[4]
-        m.suggest5 = p[5]
-        m.suggest6 = p[6]
-        m.suggest7 = p[7]
-
-        # create the directory in which to place the image
-        photo_directory = os.path.join( MEDIA_ROOT, "photos", m.slug )
-        try:
-            os.mkdir( photo_directory )
-        except OSError:
-            pass # it's okay if the directory already exists; it probably can't ever happen since slugs must be unique
-
-        # resize the image
-        photo_filename = str(m.image)
-        img_path = os.path.join( MEDIA_ROOT, photo_filename )
-        img.thumbnail( IMAGE_SIZE_BOUNDS )
-        img.save( img_path )
-
-        if commit:
-            m.save()
-
-        return m
-
+    suggest0 =   forms.CharField(  label = '',   widget = AdminGeneratedSwatchWidget )
+    suggest1 =   forms.CharField(  label = '',   widget = AdminGeneratedSwatchWidget )
+    suggest2 =   forms.CharField(  label = '',   widget = AdminGeneratedSwatchWidget )
+    suggest3 =   forms.CharField(  label = '',   widget = AdminGeneratedSwatchWidget )
+    suggest4 =   forms.CharField(  label = '',   widget = AdminGeneratedSwatchWidget )
+    suggest5 =   forms.CharField(  label = '',   widget = AdminGeneratedSwatchWidget )
+    suggest6 =   forms.CharField(  label = '',   widget = AdminGeneratedSwatchWidget )
+    suggest7 =   forms.CharField(  label = '',   widget = AdminGeneratedSwatchWidget )
 
     class Meta:
         model  = Photo
@@ -133,7 +60,6 @@ class PhotoChangeForm( PhotoAddForm ):
             "suggest6",
             "suggest7",
             "image",
-            #"text",
             )
 
 
--- src/photos/models.py
+++ src/photos/models.py
@@ -6,6 +6,9 @@ from random import randint
 from settings import ROOT_URL
 from django.db import models
 from django.forms import ModelForm
+from PIL import Image
+from settings import IMAGE_SIZE_BOUNDS, MEDIA_ROOT
+from photos.cs import palette
 
 class Photo( models.Model ):
 
@@ -20,12 +23,7 @@ class Photo( models.Model ):
         return '%sphoto/permalink/%s' % ( ROOT_URL, self.permalink )
 
     def __path__( instance, filename ):
-        try:
-            os.mkdir('./photos')
-            os.mkdir('./photos/%s' % instance.slug )
-        except OSError:
-            pass # directory already exists
-        path = 'photos/%s/%s' % ( instance.slug, filename )
+        path = os.path.join( 'photos', instance.slug, filename )
         return path
 
     def save( self, *args, **kwargs ):
@@ -35,7 +33,35 @@ class Photo( models.Model ):
         if not self.pk:
             self.permalink = md5( '%d%s' % ( randint(0,sys.maxint), datetime.datetime.now() ) ).hexdigest()
 
+        # save so that the image file will be uploaded
         super( Photo, self ).save( *args, **kwargs )
+
+        # get the image's palette
+        img = Image.open( self.image )
+        p = palette( img, 8 )
+
+        # initialize each palette color.  by default the
+        # "custom palette" is the same as the "suggested palette".
+        # each color is in hex RRGGBB format.
+        self.suggest0 = self.palette0 = p[0]
+        self.suggest1 = self.palette1 = p[1]
+        self.suggest2 = self.palette2 = p[2]
+        self.suggest3 = self.palette3 = p[3]
+        self.suggest4 = self.palette4 = p[4]
+        self.suggest5 = self.palette5 = p[5]
+        self.suggest6 = self.palette6 = p[6]
+        self.suggest7 = self.palette7 = p[7]
+
+        # save the record again to preserve the palette colors
+        super( Photo, self ).save( *args, **kwargs )
+
+        # resize the image
+        photo_filename = str(self.image)
+        img_path = os.path.join( MEDIA_ROOT, photo_filename )
+        img.thumbnail( IMAGE_SIZE_BOUNDS )
+        img.save( img_path )
+
+
 
     image         = models.ImageField( upload_to = __path__ )
 
